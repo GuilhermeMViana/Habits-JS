3b9b6233eb3ee9e03bbade7d46ae958afb84efc7
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-02-13 09:23:32 -0300

    Removendo código não utilizado

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,6 @@ const form = document.querySelector("form")
 const setupFunction = new NLWSetup(form)
 const button = document.querySelector("button")
 
-// const data = {
-//   run: ["01-01", "01-03", "01-05"],
-//   water: ["01-01", "01-02", "01-04", "01-05"],
-//   muscle: ["01-02", "01-04", "01-06"],
-//   sleep: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-//   dogCare: [],
-//   diet: [],
-// }
-
 button.addEventListener("click", () => {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = setupFunction.dayExists(today)
